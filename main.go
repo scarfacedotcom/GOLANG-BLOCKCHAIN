f5795c0b2fa4741b02accb621c5c97f0fe8f0746
scarfacedotcom <peteradaaku@gmail.com>
2023-09-23 05:11:22 +0100

    main func

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"crypto/sha256"
 )
 
+type BlockChain struct {
+	blocks []*Block
+}
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -16,6 +19,27 @@ func (b *Block) DeriveHash() {
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
 }
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
 func main() {
 
 }
